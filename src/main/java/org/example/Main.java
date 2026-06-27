41d8433b9fd9e7ce69014ea36f7411d24952a04e
Utkarsh Mishra <utkarsh.mishra1721@gmail.com>
2026-06-27 12:54:14 +0530

    First Program

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+         int[] array = {1,2,6,4,5,3};
+
+         int largest = findLargest(array);
+
+         System.out.println("Largest Element of Array is "+ largest);
+        }
+
+        static int findLargest(int[] array) {
+           int max = array[0];
+           int arrLen = array.length;
+
+           for (int i =1; i < arrLen ; i++) {
+               if (max < array[i]) {
+                   max = array[i];
+               }
+           }
+           return max;
+
+        }
+    }
